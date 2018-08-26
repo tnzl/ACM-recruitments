34a10cd1baa3dc3b78892fcecb4055a0af1bb06d
tnzl <iamtanzeel1998@gmail.com>
2018-08-27 02:00:57 +0530

    Login page design complete. Remaining: 1.Adding Listeners. 2.Adding Animation.

--- app/src/main/java/com/example/tanzeel/ACMrecruitment/MainActivity.java
+++ app/src/main/java/com/example/tanzeel/ACMrecruitment/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.tanzeel.ACMrecruitment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final double TYPE_TIME = 0.3;
+
+    private EditText mPinInput_EditTextView;
+    private ImageButton mEnterPin_Button;
+    private TextView mHashtag_TextView;
+
+    private String s;
+
+    private Timer timer = new Timer();
+
+    private String hashtag[] = {"Building the future!", "#Bit_By_Bit"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPinInput_EditTextView = (EditText) findViewById(R.id.pin_editText);
+        mEnterPin_Button = (ImageButton) findViewById(R.id.enterPin_Button);
+        mHashtag_TextView = (TextView) findViewById(R.id.hashtag_TextView);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+
     }
 }
--- app/src/main/java/com/example/tanzeel/ACMrecruitment/process.java
+++
@@ -1,5 +0,0 @@
-package com.example.tanzeel.ACMrecruitment;
-
-public class process {
-
-}
